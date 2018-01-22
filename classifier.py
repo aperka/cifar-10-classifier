4b3bdf7414a3a7cd0f4173bd0858e2945ceca8bc
Arkadiusz Perka <>
2018-01-22 22:57:30 +0100

    rbf

--- classifier.py
+++ classifier.py
@@ -4,7 +4,7 @@
 @author:
 @time:2017/3/19 11:08
 """
-from sklearn.svm import LinearSVC
+from sklearn.svm import LinearSVC, SVC
 from sklearn.externals import joblib
 import numpy as np
 import glob
@@ -12,7 +12,84 @@ import os
 import time
 from config import *
 
+classifier_cases = {
+    1 : {"type": 'linear',
+         "path": os.path.join('data', 'models', 'case1')},
+    2 : {"type": 'poly',
+         "path": os.path.join('data', 'models', 'case2'),
+         "degree": 3},
+    3: {"type": 'poly',
+        "path": os.path.join('data', 'models', 'case3'),
+        "degree": 2},
+    4: {"type": 'rbf',
+        "path": os.path.join('data', 'models', 'case4'),
+        "????": '????'},
+}
+
+def classify(train_features_file, test_features_file, classifier_case_path, classifier_case):
+    train_fds = []
+    train_labels = []
+    for img in joblib.load(train_features_file):
+        train_fds.append(img[:-1])
+        train_labels.append(img[-1])
+    print(len(img))
+
+
+    # If feature directories don't exist, create them
+    if not os.path.exists(classifier_case_path):
+        if not os.path.exists(os.path.split(classifier_case_path)[0]):
+            os.makedirs(os.path.split(classifier_case_path)[0])
+
+        if classifier_case['type'] == 'linear':
+            clf = LinearSVC(max_iter=1000)
+
+        if classifier_case['type'] == 'poly':
+            clf = SVC(kernel='poly', degree=classifier_case["degree"], max_iter=10000)
+
+        if classifier_case['type'] == 'rbf':
+            clf = SVC(kernel='rbf', max_iter=10000)
+
+        print("Training SVM Classifier.")
+        clf.fit(train_fds, train_labels)
+        joblib.dump(clf, classifier_case_path)
+    else:
+        clf = joblib.load(classifier_case_path)
+
+    num = 0
+    total = 0
+
+    for img in joblib.load(test_features_file):
+        timage_test_feat = img[:-1]
+        image_test_label = img[-1]
+
+        result = clf.predict(timage_test_feat.reshape(1, -1))
+        total += 1
+        if int(result) == int(image_test_label):
+            num += 1
+    rate = float(num)/total
+    print(classifier_case_path + ' acc: {}%'.format(rate))
+
+
+
+
 if __name__ == "__main__":
+    from threading import Thread
+
+    for classifier_case in [4]:#, 2, 3]:
+        for hog_case in [1,2,3,4,5]:
+            train_path = os.path.join("data","{}train.features".format(hog_case))
+            train_path = os.path.join("data","{}train.features".format(hog_case))
+            test_path = os.path.join("data","{}test.features".format(hog_case))
+            class_path = classifier_cases[classifier_case]['path'] + "_{}".format(hog_case)
+            Thread(target=classify, kwargs=dict(train_features_file=train_path,
+                                                test_features_file=test_path,
+                                                classifier_case_path=class_path,
+                                                classifier_case=classifier_cases[classifier_case]
+                                                )).start()
+
+
+'''
+
     t0 = time.time()
     clf_type = 'LIN_SVM'
     fds = []
@@ -23,6 +100,7 @@ if __name__ == "__main__":
         data = joblib.load(feat_path)
         fds.append(data[:-1])
         labels.append(data[-1])
+
     if clf_type is 'LIN_SVM':
         clf = LinearSVC()
         print("Training a Linear SVM Classifier.")
@@ -47,3 +125,4 @@ if __name__ == "__main__":
         t1 = time.time()
         print('The classification accuracy is %f'%rate)
         print('The cast of time is :%f'%(t1-t0))
+'''
